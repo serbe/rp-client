692e63439bb8e7c0df16de12bb84078351ae364b
Serbe <mkoctuk@gmail.com>
2019-10-07 15:55:28 +0300

    add method

---
+++ src/method.rs
@@ -0,0 +1,73 @@
+use std::str::FromStr;
+use bytes::Bytes;
+
+use crate::error::{Error, Result};
+
+#[derive(Debug)]
+pub enum Method {
+    Options,
+    Get,
+    Post,
+    Put,
+    Delete,
+    Head,
+    Trace,
+    Connect,
+    Patch,
+}
+
+impl Method {
+    pub fn as_str(&self) -> &str {
+        match self {
+            Method::Options => "OPTIONS",
+            Method::Get => "GET",
+            Method::Post => "POST",
+            Method::Put => "PUT",
+            Method::Delete => "DELETE",
+            Method::Head => "HEAD",
+            Method::Trace => "TRACE",
+            Method::Connect => "CONNECT",
+            Method::Patch => "PATCH",
+        }
+    }
+
+    pub fn from_bytes(b: Bytes) -> Result<Self> {
+        match &b[..] {
+            b"OPTIONS" => Ok(Method::Options),
+            b"GET" => Ok(Method::Get),
+            b"POST" => Ok(Method::Post),
+            b"PUT" => Ok(Method::Put),
+            b"DELETE" => Ok(Method::Delete),
+            b"HEAD" => Ok(Method::Head),
+            b"TRACE" => Ok(Method::Trace),
+            b"CONNECT" => Ok(Method::Connect),
+            b"PATCH" => Ok(Method::Patch),
+            _ => Err(Error::UnknownMethod(String::from_utf8_lossy(&b[..]).to_string()))
+        }
+    }
+}
+
+impl Default for Method {
+    fn default() -> Self {
+        Method::Get
+    }
+}
+
+impl FromStr for Method {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        match s.to_ascii_uppercase().as_str() {
+            "OPTIONS" => Ok(Method::Options),
+            "GET" => Ok(Method::Get),
+            "POST" => Ok(Method::Post),
+            "PUT" => Ok(Method::Put),
+            "DELETE" => Ok(Method::Delete),
+            "HEAD" => Ok(Method::Head),
+            "TRACE" => Ok(Method::Trace),
+            "CONNECT" => Ok(Method::Connect),
+            "PATCH" => Ok(Method::Patch),
+            _ => Err(Error::UnknownMethod(s.to_owned()))
+        }
+    }
+}
