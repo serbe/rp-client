671ca2e11af4882683a9c8a5b6625aa9d6c70475
Valeriy Kostikov <mkoctuk@gmail.com>
2019-10-07 21:47:18 +0300

    add other

--- src/scheme.rs
+++ src/scheme.rs
@@ -9,6 +9,7 @@ pub enum Scheme {
     HTTPS,
     SOCKS5,
     SOCKS5H,
+    Other(String),
 }
 
 pub trait IntoScheme {
@@ -16,18 +17,24 @@ pub trait IntoScheme {
 }
 
 impl Scheme {
-    pub fn from_opt(scheme: Option<String>) -> Result<Self> {
-        match scheme {
-            Some(scheme) => match scheme.to_lowercase().as_str() {
-                "http" => Ok(Scheme::HTTP),
-                "https" => Ok(Scheme::HTTPS),
-                "socks5" => Ok(Scheme::SOCKS5),
-                "socks5h" => Ok(Scheme::SOCKS5H),
-                _ => Err(Error::UnsupportedScheme(scheme.to_owned())),
-            },
-            None => Err(Error::UnsupportedScheme("empty scheme".to_string())),
-        }
-    }
+    // pub fn from_opt_scheme(scheme: Option<String>) -> Result<Self> {
+    //     match scheme {
+    //         Some(scheme) => match scheme.to_lowercase().as_str() {
+    //             "http" => Ok(Scheme::HTTP),
+    //             "https" => Ok(Scheme::HTTPS),
+    //             "socks5" => Ok(Scheme::SOCKS5),
+    //             "socks5h" => Ok(Scheme::SOCKS5H),
+    //             _ => {
+    //                 if scheme.chars().all(|c| c.is_alphanumeric()) {
+    //                 Ok(Scheme::Other(scheme.to_lowercase()))
+    //                 } else {
+    //                     Err(Error::UnsupportedScheme(scheme.to_owned()))
+    //                 }
+    //             }
+    //         },
+    //         None => Err(Error::UnsupportedScheme("empty scheme".to_string())),
+    //     }
+    // }
 
     pub fn as_str(&self) -> &str {
         match self {
@@ -35,15 +42,17 @@ impl Scheme {
             Scheme::HTTPS => "https",
             Scheme::SOCKS5 => "socks5",
             Scheme::SOCKS5H => "socks5h",
+            Scheme::Other(s) => s.as_str(),
         }
     }
 
-    pub fn default_port(&self) -> u16 {
+    pub fn default_port(&self) -> Option<u16> {
         match self {
-            Scheme::HTTP => 80,
-            Scheme::HTTPS => 443,
-            Scheme::SOCKS5 => 1080,
-            Scheme::SOCKS5H => 1080,
+            Scheme::HTTP => Some(80),
+            Scheme::HTTPS => Some(443),
+            Scheme::SOCKS5 => Some(1080),
+            Scheme::SOCKS5H => Some(1080),
+            Scheme::Other(s) => None,
         }
     }
 }
@@ -60,6 +69,12 @@ impl<'a> IntoScheme for &'a str {
     }
 }
 
+impl<'a> IntoScheme for String {
+    fn into_scheme(self) -> Result<Scheme> {
+        Scheme::from_str(&self)
+    }
+}
+
 impl FromStr for Scheme {
     type Err = Error;
 
@@ -69,7 +84,13 @@ impl FromStr for Scheme {
             "https" => Ok(Scheme::HTTPS),
             "socks5" => Ok(Scheme::SOCKS5),
             "socks5h" => Ok(Scheme::SOCKS5H),
-            _ => Err(Error::UnsupportedScheme(s.to_owned())),
+            _ => {
+                if s.chars().all(|c| c.is_alphanumeric()) {
+                    Ok(Scheme::Other(s.to_lowercase()))
+                } else {
+                    Err(Error::UnsupportedScheme(s.to_owned()))
+                }
+            },
         }
     }
 }
@@ -81,6 +102,7 @@ impl fmt::Display for Scheme {
             Scheme::HTTPS => write!(f, "https"),
             Scheme::SOCKS5 => write!(f, "socks5"),
             Scheme::SOCKS5H => write!(f, "socks5h"),
+            Scheme::Other(s) => write!(f, "{}", s),
         }
     }
 }
