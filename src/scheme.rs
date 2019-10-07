ab976dcc1fb4c729c3b497a4aaa0382549c09603
Serbe <mkoctuk@gmail.com>
2019-10-07 15:55:13 +0300

    more impl

--- src/scheme.rs
+++ src/scheme.rs
@@ -1,3 +1,6 @@
+use std::str::FromStr;
+use std::fmt;
+
 use crate::error::{Error, Result};
 
 #[derive(Clone, Debug)]
@@ -13,16 +16,6 @@ pub trait IntoScheme {
 }
 
 impl Scheme {
-    pub fn from_str(s: &str) -> Result<Self> {
-        match s.to_lowercase().as_str() {
-            "http" => Ok(Scheme::HTTP),
-            "https" => Ok(Scheme::HTTPS),
-            "socks5" => Ok(Scheme::SOCKS5),
-            "socks5h" => Ok(Scheme::SOCKS5H),
-            _ => Err(Error::UnsupportedScheme(s.to_owned())),
-        }
-    }
-
     pub fn from_opt(scheme: Option<String>) -> Result<Self> {
         match scheme {
             Some(scheme) => match scheme.to_lowercase().as_str() {
@@ -45,15 +38,6 @@ impl Scheme {
         }
     }
 
-    pub fn to_string(&self) -> String {
-        match self {
-            Scheme::HTTP => "http".to_string(),
-            Scheme::HTTPS => "https".to_string(),
-            Scheme::SOCKS5 => "socks5".to_string(),
-            Scheme::SOCKS5H => "socks5h".to_string(),
-        }
-    }
-
     pub fn default_port(&self) -> u16 {
         match self {
             Scheme::HTTP => 80,
@@ -64,8 +48,39 @@ impl Scheme {
     }
 }
 
+impl Default for Scheme {
+    fn default() -> Self {
+        Scheme::HTTP
+    }
+}
+
 impl<'a> IntoScheme for &'a str {
     fn into_scheme(self) -> Result<Scheme> {
         Scheme::from_str(self)
+    }
+}
+
+impl FromStr for Scheme {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        match s.to_lowercase().as_str() {
+            "http" => Ok(Scheme::HTTP),
+            "https" => Ok(Scheme::HTTPS),
+            "socks5" => Ok(Scheme::SOCKS5),
+            "socks5h" => Ok(Scheme::SOCKS5H),
+            _ => Err(Error::UnsupportedScheme(s.to_owned())),
+        }
+    }
+}
+
+impl fmt::Display for Scheme {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Scheme::HTTP => write!(f, "http"),
+            Scheme::HTTPS => write!(f, "https"),
+            Scheme::SOCKS5 => write!(f, "socks5"),
+            Scheme::SOCKS5H => write!(f, "socks5h"),
+        }
     }
 }
