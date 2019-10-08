91bc8f5faf37baf0797b249f16a248e1425cd076
Serbe <mkoctuk@gmail.com>
2019-10-08 13:56:42 +0300

    fix parse url

--- src/url.rs
+++ src/url.rs
@@ -53,22 +53,6 @@ pub struct Url {
     // pub addr: Addr,
 }
 
-fn from_split(split: Vec<&str>) -> (&str, Option<String>) {
-    if split.len() == 2 {
-        (split[0], Some(split[1].to_owned()))
-    } else {
-        (split[0], None)
-    }
-}
-
-fn from_split_rev(split: Vec<&str>) -> (&str, Option<String>) {
-    if split.len() == 2 {
-        (split[1], Some(split[0].to_owned()))
-    } else {
-        (split[0], None)
-    }
-}
-
 impl Url {
     // pub fn new(uri: Uri) -> Self {
     //     Url { uri }
@@ -118,6 +102,30 @@ impl Url {
         Ok(self.socket_addrs()?[0])
     }
 
+    pub fn decode_path(&self) -> Option<String> {
+        if let Some(path) = &self.path {
+            Some(unescape(&path))
+        } else {
+            None
+        }
+    }
+
+    pub fn decode_user(&self) -> Option<String> {
+        if let Some(userinfo) = &self.userinfo {
+            Some(unescape(&userinfo.username))
+        } else {
+            None
+        }
+    }
+
+    pub fn decode_password(&self) -> Option<String> {
+        if let Some(userinfo) = &self.userinfo {
+            Some(unescape(&userinfo.password))
+        } else {
+            None
+        }
+    }
+
     //     pub fn uri(&self) -> Uri {
     //         self.uri.clone()
     //     }
@@ -133,20 +141,29 @@ impl FromStr for Url {
     fn from_str(s: &str) -> Result<Self> {
         let raw = s;
 
-        let (raw, fragment) = from_split(raw.rsplitn(2, '#').collect());
-        let (raw, query) = from_split(raw.rsplitn(2, '?').collect());
-        let (raw, scheme) = from_split_rev(raw.splitn(2, "://").collect());
+        let (raw, fragment) = from_split_rev(raw.rsplitn(2, '#').collect());
+        let (raw, query) = from_split(raw.splitn(2, '?').collect());
+        let (raw, scheme) = from_split_rev(raw.splitn(2, ':').collect());
         let scheme = match scheme {
             Some(scheme) => Some(Scheme::from_str(&scheme)?),
             None => None,
         };
-        let (raw, userinfo) = from_split_rev(raw.splitn(2, '@').collect());
+        let (raw, userinfo) = if raw.starts_with("//") {
+            from_split_rev(raw[2..].splitn(2, '@').collect())
+        } else {
+            from_split_rev(raw.splitn(2, '@').collect())
+        };
         let userinfo = if let Some(user_info) = userinfo {
             Some(user_info.into())
         } else {
             None
         };
-        let (raw, path) = from_split(raw.splitn(2, '/').collect());
+        let (raw, path) = if let Some(pos) = raw.find('/') {
+            let (raw, path) = raw.split_at(pos);
+            (raw, Some(path.to_owned()))
+        } else {
+            (raw, None)
+        };
 
         let (host, port) = if let Some(pos) = raw.rfind(':') {
             if let Some(start) = raw.find('[') {
@@ -232,6 +249,48 @@ impl FromStr for Url {
 //     url.as_str().parse().ok()
 // }
 
+fn from_split(split: Vec<&str>) -> (&str, Option<String>) {
+    if split.len() == 2 {
+        (split[0], Some(split[1].to_owned()))
+    } else {
+        (split[0], None)
+    }
+}
+
+fn from_split_rev(split: Vec<&str>) -> (&str, Option<String>) {
+    if split.len() == 2 {
+        (split[1], Some(split[0].to_owned()))
+    } else {
+        (split[0], None)
+    }
+}
+
+fn unescape(s: &str) -> String {
+    if s.len() < 3 {
+        s.to_owned()
+    } else {
+        let mut result = String::new();
+        let v = s.chars().collect::<Vec<char>>();
+        let mut i = 0usize;
+        while i < s.len() {
+            if i < s.len() - 2
+                && v[i] == '%'
+                && v[i + 1].is_ascii_hexdigit()
+                && v[i + 2].is_ascii_hexdigit()
+            {
+                let n1 = u8::from_str_radix(&v[i + 1].to_string(), 16).unwrap();
+                let n2 = u8::from_str_radix(&v[i + 2].to_string(), 16).unwrap();
+                result.push(char::from(n1 * 16 + n2));
+                i += 3;
+            } else {
+                result.push(v[i]);
+                i += 1;
+            }
+        }
+        result
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -255,16 +314,16 @@ mod tests {
         assert_eq!(s, u);
     }
 
-    // #[test]
-    // fn path_with_hex_escaping() {
-    // 	let mut u = Url::default();
-    // 	let s = Url::from_str("http://www.example.org/file%20one%26two").unwrap();
-    // 	u.scheme = Some(Scheme::HTTP);
-    // 	u.host = "www.example.org";
-    // 	// u.path = Some("/file one&two");
-    // 	u.path = Some("/file%20one%26two");
-    // 	assert_eq!(s, u);
-    // }
+    #[test]
+    fn path_with_hex_escaping() {
+    	let mut u = Url::default();
+    	let s = Url::from_str("http://www.example.org/file%20one%26two").unwrap();
+    	u.scheme = Some(Scheme::HTTP);
+    	u.host = "www.example.org".to_owned();
+    	u.path = Some("/file%20one%26two".to_owned());
+        assert_eq!(u.decode_path(), Some("/file one&two".to_owned()));
+    	assert_eq!(s, u);
+    }
 
     #[test]
     fn user() {
@@ -280,21 +339,20 @@ mod tests {
         assert_eq!(s, u);
     }
 
-    // #[test]
-    // fn escape_sequence_in_username() {
-    // 	let mut u = Url::default();
-    // 	let s = Url::from_str("ftp://john%20doe@www.example.org/").unwrap();
-    // 	u.scheme = Some("ftp");
-    // 	// u.user = Some("john doe");
-    // 	u.user = Some("john%20doe");
-    // u.userinfo = Some(UserInfo {
-    //         username: "webmaster".to_owned(),
-    //         password: String::new(),
-    //     });
-    // 	u.host = "www.example.org";
-    // 	u.path = Some("/".to_owned());
-    // 	assert_eq!(s, u);
-    // }
+    #[test]
+    fn escape_sequence_in_username() {
+    	let mut u = Url::default();
+    	let s = Url::from_str("ftp://john%20doe@www.example.org/").unwrap();
+    	u.scheme = Some(Scheme::Other("ftp".to_owned()));
+        u.userinfo = Some(UserInfo {
+            username: "john%20doe".to_owned(),
+            password: String::new(),
+        });
+    	u.host = "www.example.org".to_owned();
+    	u.path = Some("/".to_owned());
+        assert_eq!(u.decode_user(), Some("john doe".to_owned()));
+    	assert_eq!(s, u);
+    }
 
     #[test]
     fn empty_query() {
@@ -329,27 +387,28 @@ mod tests {
         assert_eq!(s, u);
     }
 
-    // #[test]
-    // fn query_with_hex_escaping() {
-    //     let mut u = Url::default();
-    //     let s = Url::from_str("http://www.example.org/?q=go%20language").unwrap();
-    //     u.scheme = Some(Scheme::HTTP);
-    //     u.host = "www.example.org".to_owned();
-    //     u.path = Some("/".to_owned());
-    //     u.query = Some("q=go%20language");
-    //     assert_eq!(s, u);
-    // }
+    #[test]
+    fn query_with_hex_escaping() {
+        let mut u = Url::default();
+        let s = Url::from_str("http://www.example.org/?q=go%20language").unwrap();
+        u.scheme = Some(Scheme::HTTP);
+        u.host = "www.example.org".to_owned();
+        u.path = Some("/".to_owned());
+        u.query = Some("q=go%20language".to_owned());
+        assert_eq!(s, u);
+    }
 
-    // #[test]
-    // fn outside_query() {
-    //     let mut u = Url::default();
-    //     let s = Url::from_str("http://www.example.org/a%20b?q=c+d").unwrap();
-    //     u.scheme = Some(Scheme::HTTP);
-    //     u.host = "www.example.org".to_owned();
-    //     u.path = Some("/a b");
-    //     u.query = Some("q=c+d");
-    //     assert_eq!(s, u);
-    // }
+    #[test]
+    fn outside_query() {
+        let mut u = Url::default();
+        let s = Url::from_str("http://www.example.org/a%20b?q=c+d").unwrap();
+        u.scheme = Some(Scheme::HTTP);
+        u.host = "www.example.org".to_owned();
+        u.path = Some("/a%20b".to_owned());
+        u.query = Some("q=c+d".to_owned());
+        assert_eq!(s.decode_path(), Some("/a b".to_owned()));
+        assert_eq!(s, u);
+    }
 
     #[test]
     fn path_without_leading2() {
@@ -428,24 +487,24 @@ mod tests {
     // fn relative_path() {
     // 	let mut u = Url::default();
     // 	let s = Url::from_str("a/b/c").unwrap();
-    // 	u.path = Some("a/b/c");
+    // 	u.path = Some("a/b/c".to_owned());
     // 	assert_eq!(s, u);
     // }
 
-    // #[test]
-    // fn escaped() {
-    //     let mut u = Url::default();
-    //     let s = Url::from_str("http://%3Fam:pa%3Fsword@google.com").unwrap();
-    //     u.scheme = Some(Scheme::HTTP);
-    // u.userinfo = Some(UserInfo {
-    //     username: "webmaster".to_owned(),
-    //     password: String::new(),
-    // });
-    //     u.user = Some("?am");
-    //     u.password = Some("pa?sword");
-    //     u.host = "google.com";
-    //     assert_eq!(s, u);
-    // }
+    #[test]
+    fn escaped() {
+        let mut u = Url::default();
+        let s = Url::from_str("http://%3Fam:pa%3Fsword@google.com").unwrap();
+        u.scheme = Some(Scheme::HTTP);
+        u.userinfo = Some(UserInfo {
+            username: "%3Fam".to_owned(),
+            password: "pa%3Fsword".to_owned(),
+        });
+        u.host = "google.com".to_owned();
+        assert_eq!(s.decode_user(), Some("?am".to_owned()));
+        assert_eq!(s.decode_password(), Some("pa?sword".to_owned()));
+        assert_eq!(s, u);
+    }
 
     #[test]
     fn host_subcomponent() {
@@ -531,17 +590,17 @@ mod tests {
     //     assert_eq!(s, u);
     // }
 
-    // #[test]
-    // fn alternate_escapings_of_path_survive_round_trip() {
-    //     let mut u = Url::default();
-    //     let s = Url::from_str("http://rest.rsc.io/foo%2fbar/baz%2Fquux?alt=media").unwrap();
-    //     u.scheme = Some(Scheme::HTTP);
-    //     u.host = "rest.rsc.io";
-    //     u.path = Some("/foo/bar/baz/quux");
-    //     // Rawu.path = Some("/foo%2fbar/baz%2Fquux");
-    //     u.query = Some("alt=media");
-    //     assert_eq!(s, u);
-    // }
+    #[test]
+    fn alternate_escapings_of_path_survive_round_trip() {
+        let mut u = Url::default();
+        let s = Url::from_str("http://rest.rsc.io/foo%2fbar/baz%2Fquux?alt=media").unwrap();
+        u.scheme = Some(Scheme::HTTP);
+        u.host = "rest.rsc.io".to_owned();
+        u.path = Some("/foo%2fbar/baz%2Fquux".to_owned());
+        u.query = Some("alt=media".to_owned());
+        assert_eq!(s.decode_path(), Some("/foo/bar/baz/quux".to_owned()));
+        assert_eq!(s, u);
+    }
 
     #[test]
     fn issue_12036() {
@@ -553,24 +612,24 @@ mod tests {
         assert_eq!(s, u);
     }
 
-    // #[test]
-    // fn worst_case_host() {
-    //     let mut u = Url::default();
-    //     let s = Url::from_str("scheme://!$&'()*+,;=hello!:port/path").unwrap();
-    //     u.scheme = Some("scheme");
-    //     u.host = "!$&'()*+,;=hello!";
-    //     u.port = Some(":port");
-    //     u.path = Some("/path");
-    //     assert_eq!(s, u);
-    // }
+    #[test]
+    fn worst_case_host() {
+        let mut u = Url::default();
+        let s = Url::from_str("scheme://!$&'()*+,;=hello!:23/path").unwrap();
+        u.scheme = Some(Scheme::Other("scheme".to_owned()));
+        u.host = "!$&'()*+,;=hello!".to_owned();
+        u.port = Some(23);
+        u.path = Some("/path".to_owned());
+        assert_eq!(s, u);
+    }
 
     // #[test]
     // fn worst_case_path() {
     //     let mut u = Url::default();
     //     let s = Url::from_str("http://host/!$&'()*+,;=:@[hello]").unwrap();
     //     u.scheme = Some(Scheme::HTTP);
-    //     u.host = "host";
-    //     u.path = Some("/!$&'()*+,;=:@[hello]");
+    //     u.host = "host".to_owned();
+    //     u.path = Some("/!$&'()*+,;=:@[hello]".to_owned());
     //     // Rawu.path = Some("/!$&'()*+,;=:@[hello]");
     //     assert_eq!(s, u);
     // }
@@ -623,11 +682,13 @@ mod tests {
     //      assert_eq!(s, u);
     // }
 
+    // #[test]
+    // fn ipv6_2() {
     //      let mut u = Url::default();
     //      let s = Url::from_str("http:[2b01:e34:ef40:7730:8e70:5aff:fefe:edac]:8080/foo").unwrap();
     //      		u.scheme = Some(Scheme::HTTP);
-    //      		u.host = "[2b01:e34:ef40:7730:8e70:5aff:fefe:edac]:8080";
-    //      		u.path = Some("/foo");
+    //      		u.host = "[2b01:e34:ef40:7730:8e70:5aff:fefe:edac]:8080".to_owned();
+    //      		u.path = Some("/foo".to_owned());
     //      assert_eq!(s, u);
     // }
 
